cf728dec005b3a3a36bb7397799c738d76b3a5c6
Infomod1 <159074946+Infomod1@users.noreply.github.com>
2024-02-05 22:08:25 +0100

    Add files via upload

---
+++ BeaGIT.java
@@ -0,0 +1,34 @@
+package bea.git;
+import java.util.Scanner;
+/**
+ *
+ * @author Bea
+ */
+public class BeaGIT {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a,b,c;
+        System.out.print("Ingrese un numero: ");
+        a = sc.nextInt();
+        System.out.print("Ingrese otro numero: ");
+        b = sc.nextInt();
+        c = a+b;
+            System.out.println("La suma de " + a + " y " + b + " es: " + c);
+            System.out.println("Realizado por Beatriz");
+        sc.close();
+
+        // Ejemplo de uso
+        char[][] matriz = {
+            {'h', 'o', 'l', 'a'},
+            {'m', 'u', 'n', 'd', 'o'},
+            {'p', 'r', 'o', 'g', 'r', 'a', 'm', 'a', 'r'}
+        };
+        
+        String[] palabras = {"hola", "mundo", "java"};
+	System.out.println("Cambio en RAMA2");
+
+        int cantidad = palabrasMatriz(matriz, palabras);
+        System.out.println("Cantidad de palabras encontradas: " + cantidad);
+    }
+}
